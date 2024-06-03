50dc311921ca800bd70d14221f3c54afc33816fe
Eichi2514 <picon1317@gmail.com>
2024-06-03 15:19:59 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("이름: " + 이름);
-        System.out.println("나이: " + 나이);
-        System.out.println("키 :" + 키);
+        객체 변수 = new 객체();
+
+        변수.성 = "유";
+        변수.이름 = "건혁";
+        변수.나이 = 29;
+        변수.키 = 180.7;
+
+        System.out.println("이름 : " + 변수.성 + " " + 변수.이름);
+        System.out.println("나이 : " + 변수.나이);
+        System.out.println("키 : " + 변수.키);
+    }
     }
 
-    class 건혁 {
+    class 객체 {
+        String 성;
         String 이름;
         int 나이;
         double 키;
-
-        건혁(String 이름, int 나이, double 키);
-
-        이름 ="유건혁";
-        나이 =29;
-        키 =180.7;
     }
-}
