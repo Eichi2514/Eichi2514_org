1c3c188c81207394b6b31232c0f95545a37f6eca
Eichi2514 <picon1317@gmail.com>
2024-06-03 15:04:28 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.printf("hi!");
-        }
+
+        System.out.println("이름 : " + 이름);
+        System.out.println("나이 : " + 나이);
+        System.out.println("키 : " + 키);
     }
+
+    class 건혁 {
+        String 이름;
+        int 나이;
+        double 키;
+
+        건혁(String 이름, int 나이, double 키);
+
+        이름 ="유건혁";
+        나이 =29;
+        키 =180.7;
+    }
+}
