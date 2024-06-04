dd2386a19b357323c74dbb9355fa21b90616bf92
Eichi2514 <picon1317@gmail.com>
2024-06-04 13:02:48 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,36 +3,45 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
+
         사람 a = new 사람();
         a.나이 = 23;
-        a.이름 = "김철수";
+        a.성 = "김";
+        a.이름 = "철수";
         a.결혼여부 = false;
         a.hello();
 
-        사람 b = new 사람();
-        b.나이 = 33;
-        b.이름 = "박영수";
-        b.결혼여부 = true;
-        b.hello();
+        a++;
 
-        사람 c = new 사람();
-        c.나이 = 28;
-        c.이름 = "이재용";
-        c.결혼여부 = true;
-        c.hello();
+        사람 a = new 사람();
+        a.나이 = 33;
+        a.성 = "박";
+        a.이름 = "영수";
+        a.결혼여부 = true;
+        a.hello();
+
+        a++;
+
+        사람 a = new 사람();
+        a.나이 = 28;
+        a.성 = "이";
+        a.이름 = "재용";
+        a.결혼여부 = true;
+        a.hello();
     }
 }
 
 class 사람 {
+    String 성;
     String 이름;
     int 나이;
     boolean 결혼여부;
 
     void hello() {
         System.out.println(이름 + "의 프로필");
-        System.out.println("이름 : " + 이름);
-        System.out.println("나이 : " + 나이);
-        System.out.println("결혼여부 : " + 결혼여부);
+        System.out.println("이름 : " + this.성 + this.이름);
+        System.out.println("나이 : " + this.나이);
+        System.out.println("결혼여부 : " + this.결혼여부);
         System.out.println();
     }
 }
