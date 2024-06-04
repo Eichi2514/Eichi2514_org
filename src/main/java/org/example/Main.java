629f343f0fe3245022666994a51db6c018859594
Eichi2514 <picon1317@gmail.com>
2024-06-04 10:03:21 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,38 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
+        사람 a = new 사람();
+        a.나이 = 23;
+        a.이름 = "김철수";
+        a.결혼여부 = false;
+        a.hello();
 
-        자동차 a = new 자동차();
-        a.최고속력 = 240;
-        자동차 b = new 자동차();
-        b.최고속력 = 260;
+        사람 b = new 사람();
+        b.나이 = 33;
+        b.이름 = "박영수";
+        b.결혼여부 = true;
+        b.hello();
 
-        System.out.println(a.최고속력);
-        System.out.println(b.최고속력);
-
+        사람 c = new 사람();
+        c.나이 = 28;
+        c.이름 = "이재용";
+        c.결혼여부 = true;
+        c.hello();
     }
+}
 
-    static class 자동차 {
-        String 이름;
-        String 색깔;
-        int 번호;
-        int 최고속력;
+class 사람 {
+    String 이름;
+    int 나이;
+    boolean 결혼여부;
+
+    void hello() {
+        System.out.println(이름 + "의 프로필");
+        System.out.println("이름 : " + 이름);
+        System.out.println("나이 : " + 나이);
+        System.out.println("결혼여부 : " + 결혼여부);
+        System.out.println();
     }
 }
